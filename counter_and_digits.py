cae8f650269b5cc498140bd44f75ecf494d5b4a6
DaolecongAn <91931795+Mimlot@users.noreply.github.com>
2024-12-09 10:30:02 +0700

    up

--- counter_and_digits.py
+++ counter_and_digits.py
@@ -1,13 +1,19 @@
 from collections import Counter
 
 def count_num(day):
-    so =[i for i in day if i.isdigit()]
-    return so
+    #so =[i for i in day if i.isdigit()]
+    dem = 0
+    for i in day:
+        if i.isdigit():
+            dem += 1
+    return dem
 def count_words(day):
     return Counter(day)
 
 def main ():
     day = list(input("Nhap day ky tu khac nhau : "))
     print(count_num(day))
-    print(count_words(day))
+    d = count_words(day)
+    for i in d:
+        print(f"{i} xuat hien {d[i]} lan")
 main()

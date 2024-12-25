6021c64962c7a64a0f071d49177e0cd3cdba9bbd
DaolecongAn <91931795+Mimlot@users.noreply.github.com>
2024-12-25 21:18:10 +0700

    update

--- opencv_pract/dectechtio.py
+++ opencv_pract/dectechtio.py
@@ -6,8 +6,7 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 harr_cascade = cv.CascadeClassifier('opencv_pract/harr.xml')
 
 face_rectang = harr_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors= 3)
-print(f"So khuon mat trong hinh: {len(gray)}")
-
+print(f"So khuon mat trong hinh: {len(face_rectang)}")
 for (x,y,w,h) in face_rectang:
     cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
 cv.imshow('Face', img)
